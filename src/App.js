17dede6efbda38958022163249952dcac0e65232
Leila <leila.alhemali@gmail.com>
2022-10-12 14:26:40 -0700

    added gh pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="https://live.staticflickr.com/65535/52420958302_004734700a_o.jpg"
+        width="450"
+        height="800"
+        alt="20210528_154215"
+      />
+      <img
+        src="https://live.staticflickr.com/65535/52421983103_a90c164ebb_q.jpg"
+        width="450"
+        height="800"
+        alt="20210528_151446"
+      />
+      <img
+        src="https://live.staticflickr.com/65535/52421983103_a90c164ebb_c.jpg"
+        width="800"
+        height="450"
+        alt="20210528_151446"
+      />
+      <img
+        src="https://live.staticflickr.com/65535/52421803649_f8f3f089fa_c.jpg"
+        width="800"
+        height="600"
+        alt="IMG-20221011-WA0010"
+      />
     </div>
   );
 }
